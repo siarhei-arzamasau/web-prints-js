d70d7cfcbf2b35751e2db04ed72e1247679d3efa
Petr Lipatov <lipatov3@gmail.com>
2024-09-28 09:10:38 +0300

    feat: Layout route added

--- client/src/app/routers/appRouter.tsx
+++ client/src/app/routers/appRouter.tsx
@@ -2,13 +2,16 @@ import {Routes, Route, BrowserRouter} from 'react-router-dom';
 
 import {EditorPage} from '@/pages/editor';
 import {HomePage} from '@/pages/home';
+import {Layout} from '../layout/layout';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='editor' element={<EditorPage />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path='editor' element={<EditorPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
