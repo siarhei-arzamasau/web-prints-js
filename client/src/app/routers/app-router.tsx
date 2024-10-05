7bd38a74ab999e3ce805d9416f97935e51a2b2b1
Petr Lipatov <lipatov3@gmail.com>
2024-10-05 19:09:55 +0300

    fix: Dynamic key value added

--- client/src/app/routers/app-router.tsx
+++ client/src/app/routers/app-router.tsx
@@ -3,15 +3,16 @@ import {appRoutes} from './routes';
 import {RouterPaths} from '@/shared/consts';
 import {AppRoute} from './types';
 
-const renderRoutes = (routes: AppRoute[]) => {
+const renderRoutes = (routes: AppRoute[], parentPath = '') => {
   return routes.map(({path, element, children}) => {
+    const fullPath = parentPath ? `${parentPath}-${path}` : path;
     const isIndexRoute = path === RouterPaths.Index;
 
     return isIndexRoute ? (
-      <Route index element={element} key={path} />
+      <Route index element={element} key={fullPath} />
     ) : (
-      <Route path={path} element={element} key={path}>
-        {children && renderRoutes(children)}
+      <Route path={path} element={element} key={fullPath}>
+        {children && renderRoutes(children, fullPath)}
       </Route>
     );
   });
--- client/src/app/routers/routes.tsx
+++ client/src/app/routers/routes.tsx
@@ -11,7 +11,16 @@ export const appRoutes: AppRoute[] = [
     isPublic: true,
     children: [
       {path: RouterPaths.Index, element: <HomePage />, isPublic: true},
-      {path: RouterPaths.Editor, element: <EditorPage />, isPublic: true},
+      {
+        path: RouterPaths.Editor,
+        element: <EditorPage />,
+        isPublic: true,
+        children: [
+          {path: 'index', element: <HomePage />, isPublic: true},
+          {path: '/editor/lol', element: <HomePage />, isPublic: true},
+          {path: '/editor/2', element: <EditorPage />, isPublic: true},
+        ],
+      },
     ],
   },
 ];
